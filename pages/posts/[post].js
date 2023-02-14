1590066c02b14e206afe27c1283e201756872d70
RAHAT HASAN DIPO <rahathasandipto@gmail.com>
2023-02-14 15:27:27 +0600

    This is the getStaticPath

--- pages/posts/[post].js
+++ pages/posts/[post].js
@@ -1,10 +1,36 @@
-function post() {
+
+export async function getStaticPaths() {
+    const res = await fetch('https://jsonplaceholder.typicode.com/posts')
+    const posts = await res.json();
+
+    const paths = posts.map((post) => ({
+        params: { post: post.id.toString() },
+    }));
+    return {
+        paths,
+        fallback: false
+    }
+}
+
+export async function getStaticProps({ params }) {
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${params.post}`)
+    const post = await res.json()
+
+    return { props: { post } }
+};
+
+
+function id({ post }) {
+    console.log(post);
     return (
         <div>
-            <h1>Lorem ipsum dolor sit.</h1>
+            <p className="text-center text-3xl">{post.id}</p>
+            <h4 className="text-center"> Title{post.title}</h4>
+
+
             Enter
         </div>
     );
 }
 
-export default post;
+export default id;
--- pages/products/[product].js
+++ pages/products/[product].js
@@ -1,7 +1,26 @@
+export async function getStaticPaths() {
+    const res = await fetch('https://fakestoreapi.com/products')
+    const posts = await res.json()
+
+    const paths = posts.map((post) => ({
+        params: { id: post.id },
+    }))
+
+    return { paths,
+         fallback: false }
+}
+
+export async function getStaticProps({ params }) {
+    const res = await fetch(`https://fakestoreapi.com/products/${params.id}`)
+    const post = await res.json()
+
+    return { props: { post } }
+}
+
+
 function product() {
     return (
         <div>
-            <h1>Lorem ipsum dolor sit amet.</h1>
             Enter
         </div>
     );
