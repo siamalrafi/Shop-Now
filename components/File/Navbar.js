7d60e458eba7d00f4ff083391ee5a6dcf77ffbdb
RAHAT HASAN DIPO <rahathasandipto@gmail.com>
2023-02-14 16:06:21 +0600

    This is hte next

--- components/File/Navbar.js
+++ components/File/Navbar.js
@@ -21,10 +21,10 @@ function Navbar() {
                         </label>
                         <ul tabIndex={0} className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52">
                             <li>
-                                <a className="justify-between">
+                                <Link href={'/profile'} className="justify-between">
                                     Profile
                                     <span className="badge">New</span>
-                                </a>
+                                </Link>
                             </li>
                             <li><a>Settings</a></li>
                             <li><a>Logout</a></li>
---
+++ pages/profile.js
@@ -0,0 +1,10 @@
+function profile() {
+    return (
+        <div>
+            <h1>this is the profile page.</h1>
+            Enter
+        </div>
+    );
+}
+
+export default profile;
